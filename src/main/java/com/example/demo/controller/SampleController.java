6a43a2cb2bb95197e87f00d58553819c2a7c9240
ZzinB <mysteleee7@gmail.com>
2024-08-06 18:04:48 +0900

    Feat: 컨트롤러

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,15 +1,54 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.SampleDTO;
+import com.example.demo.util.LocalUploader;
+import com.example.demo.util.S3Uploader;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/sample")
+@Log4j2
+@RequiredArgsConstructor
 public class SampleController {
 
-    @GetMapping("/getArr")
-    public String[] getArr(){
-        return new String[]{"AAA", "BBB", "CCC"};
+    private final LocalUploader localUploader;
+
+    private final S3Uploader s3Uploader;
+
+    @PostMapping("/upload")
+    public ResponseEntity<List<String>> upload(@RequestParam("files") MultipartFile[] files) {
+        try {
+            if (files == null || files.length <= 0) {
+                return ResponseEntity.badRequest().body(Collections.emptyList());
+            }
+
+            List<String> uploadedFilePaths = new ArrayList<>();
+
+            for (MultipartFile file : files) {
+                uploadedFilePaths.addAll(localUploader.uploadLocal(file));
+            }
+            log.info("--------------------");
+            log.info(uploadedFilePaths);
+
+            List<String> s3Paths = uploadedFilePaths.stream()
+                    .map(fileName -> s3Uploader.upload(fileName))
+                    .collect(Collectors.toList());
+
+            return ResponseEntity.ok(s3Paths);
+        } catch (Exception e) {
+            log.error("Exception during file upload: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
+
 }
